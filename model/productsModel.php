44d9ea1d54f2108da1f0dd8fc7aa3029becd2d91
Fredrikhietala <hietala.fredrik@gmail.com>
2017-04-10 14:27:52 +0200

    Added a update-function

--- model/productsModel.php
+++ model/productsModel.php
@@ -1,27 +1,40 @@
 <?php
-//require "dbconnect.php";
+require "movies.php";
+
+$movies = new Movies();
 
 class Crud {
-	public $connection;
+	private $db;
 
-	public function __construct(PDO $connection = null) {
-		$this->connection = $connection;
-		if ($this->connection === null) {
-			$this->connection = new PDO (
-				'mysql:host=localhost;dbname=movies',
-				'root',
-				'root'
-				);
-			$this->connection->setAttribute(
-				PDO::ATTR_ERRMODE,
-				PDO::ERRMODE_EXCEPTION
-			);
-		}
+	public function __construct($connection) {
+		$this->db = $connection;
 	}
-	public function showAll() {
+
+	public function readAll() {
 		$sql = "SELECT * FROM `films`";
-		$stm = $this->connection->prepare($sql);
+		$stm = $this->db->prepare($sql);
 		$stm->execute();
+		$stm->setFetchMode(PDO::FETCH_CLASS, 'Movies');
 		return $stm->fetchAll();
+	}
+
+	public function getId($id) {
+		$sql = "SELECT * FROM `films` WHERE `id`= :id";
+		$stm = $this->db->prepare($sql);
+		$stm->execute(['id' => $id]);
+		$editRow=$stm->fetch(PDO::FETCH_CLASS, 'Movies');
+		return $editRow;
+	}
+
+	public function update($id, $title, $altTitle, $director, $country, $year) {
+		$sql = "UPDATE `films` SET `title`=:title, `swedishTitle`=:altTitle, `director`=:director, `country`=:country, `year`=:year WHERE `id`=:id";
+		$stm = $this->db->prepare($sql);
+		$stm->bindparam(":title", $movies->title);
+		$stm->bindparam(":swedishTitle",$movies->altTitle);
+		$stm->bindparam(":director",$movies->director);
+		$stm->bindparam(":country",$movies->country);
+		$stm->bindparam(":year",$movies->year);
+		$stm->execute();
+		return true;
 	}
 }
