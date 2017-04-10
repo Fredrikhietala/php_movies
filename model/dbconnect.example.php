e48f4260cfe207671f9f3cfdedaafaebd67a8e73
Fredrikhietala <hietala.fredrik@gmail.com>
2017-04-10 11:34:50 +0200

    Added a dbconnect example-file

---
+++ model/dbconnect.example.php
@@ -0,0 +1,22 @@
+<?php
+
+	$host = ''; 
+	$db = '';
+	$user = '';
+	$password = '';
+	$charset = '';
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+	$options = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES   => false  ];
+	try { 
+	//Try to connect to database
+	$pdo = new PDO($dsn, $user, $password, $options);
+	} catch (PDOException $e){ 
+	//if not connected succesfully, give me an error message
+		echo $e->getMessage();
+	}
+
+		include_once "productsModel.php";
+
+	$crud = new Crud($connection);
