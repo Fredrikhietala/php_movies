e02a36a9305c3e73e52e5b8cb9c6306d5d8050a5
Fredrikhietala <hietala.fredrik@gmail.com>
2017-04-15 13:27:08 +0200

    Testing insert-function

--- views/create.php
+++ views/create.php
@@ -1,10 +1,29 @@
 <?php 
-//require "../model/productsModel.php";
+require "../model/productsModel.php";
 include "header.php";
+
+$insert = new MoviesCrud($connection);
+//$insertMovie = new Movies();
+
+if (isset($_POST['insert'])) {
+    $array = $movie->setTitle($_POST['title']);
+    $array = $movie->setAltTitle($_POST['altTitle']);
+    $array = $movie->setDirector($_POST['director']);
+    $array = $movie->setCountry($_POST['country']);
+    $array = $movie->setYear($_POST['year']);
+
+    if ($movies = $insert->create($array)) {
+        echo "<div class='alert alert-success'>Movie was inserted</div>";
+        header("Location: index.php");
+    } else {
+        echo "<div class='alert alert-danger'>Unable to insert product</div>";
+        header("Location: ../views/create.php");
+    }
+}
 
 ?>
 
-<form action="../index.php" method="post">
+<form action="create.php" method="post">
 	<div class="form-group">
 		<label for="title">Title: </label>
         <input type="text" name="title" class="form-control" id="title" placeholder="Title" required>
@@ -25,7 +44,7 @@ include "header.php";
 		<label for="year">Year: </label>
 		<input type="text" name="year" class="form-control" id="year" placeholder="Year" required>
 	</div>
-	<button type="submit" class="btn btn-default" name="insert">Insert new movie</button>
+	<button type="submit" class="btn btn-default" name="insert" id="insert">Insert new movie</button>
 </form>
 
 <?php
