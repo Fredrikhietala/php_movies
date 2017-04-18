778217a4cc5dcf40e1e825efd0251eccc796493e
Fredrikhietala <hietala.fredrik@gmail.com>
2017-04-18 15:18:06 +0200

    Updated update function

--- model/productsModel.php
+++ model/productsModel.php
@@ -29,20 +29,22 @@ class MoviesCrud {
 	}
 
 	public function update($id, $title, $altTitle, $director, $country, $year) {
-		$sql = 'UPDATE `films` SET `title`=:title, `altTitle`=:altTitle, `director`=:director, `country`=:country, `year`=:year WHERE `id`=:id';
-		$stm = $this->db->prepare($sql);
-		$stm->bindparam(":id", $id->movie->getId());
-		$stm->bindparam(":title", $title->movie->getTitle());
-		$stm->bindparam(":altTitle",$altTitle->movie->getAltTitle());
-		$stm->bindparam(":director",$director->movie->getDirector());
-		$stm->bindparam(":country",$country->movie->getCountry());
-		$stm->bindparam(":year",$year->movie->getYear());
-		$movies = $stm->execute();
-		return $movies;
-	}
+        $sql = 'UPDATE `films` SET `title`=:title, `altTitle`=:altTitle, `director`=:director, `country`=:country, `year`=:year WHERE `id`=:id';
+        $stm = $this->db->prepare($sql);
+        $stm->bindparam(":id", $id);
+        $stm->bindparam(":title", $title);
+        $stm->bindparam(":altTitle", $altTitle);
+        $stm->bindparam(":director", $director);
+        $stm->bindparam(":country", $country);
+        $stm->bindparam(":year", $year);
+        if (!empty($movies = $stm->execute())) {
+            return $movies;
+        }
+        else
+            return false; //@TODO
+    }
 
     public function create($title, $altTitle, $director, $country, $year) {
-	    global $movie;
 		$sql = 'INSERT INTO `films` (`title`, `altTitle`, `director`, `country`, `year`) VALUES (:title, :altTitle, :director, :country, :year)';
 		$stm = $this->db->prepare($sql);
         $stm->bindparam(":title", $title);
