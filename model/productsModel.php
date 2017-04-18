55bfbcce883eab5d38b8f22137c018dab5aa140c
Fredrikhietala <hietala.fredrik@gmail.com>
2017-04-18 08:43:38 +0200

    Added delete-query

--- model/productsModel.php
+++ model/productsModel.php
@@ -1,7 +1,7 @@
 <?php
 require "movies.php";
 
-$movie = new Movies();
+$movie = new Movies($connection);
 global $movie;
 
 class MoviesCrud {
@@ -20,7 +20,7 @@ class MoviesCrud {
 	}
 
 	public function getById($id) {
-		$sql = 'SELECT * FROM `films` WHERE `id`= :id';
+		$sql = 'SELECT * FROM `films` WHERE `id`=:id';
 		$stm = $this->db->prepare($sql);
 		$stm->bindparam(":id", $id);
 		$stm->execute();
@@ -31,12 +31,12 @@ class MoviesCrud {
 	public function update($id, $title, $altTitle, $director, $country, $year) {
 		$sql = 'UPDATE `films` SET `title`=:title, `altTitle`=:altTitle, `director`=:director, `country`=:country, `year`=:year WHERE `id`=:id';
 		$stm = $this->db->prepare($sql);
-		$stm->bindparam(":id", $id->getId());
-		$stm->bindparam(":title", $title->getTitle());
-		$stm->bindparam(":altTitle",$altTitle->getAltTitle());
-		$stm->bindparam(":director",$director->getDirector());
-		$stm->bindparam(":country",$country->getCountry());
-		$stm->bindparam(":year",$year->getYear());
+		$stm->bindparam(":id", $id->movie->getId());
+		$stm->bindparam(":title", $title->movie->getTitle());
+		$stm->bindparam(":altTitle",$altTitle->movie->getAltTitle());
+		$stm->bindparam(":director",$director->movie->getDirector());
+		$stm->bindparam(":country",$country->movie->getCountry());
+		$stm->bindparam(":year",$year->movie->getYear());
 		$movies = $stm->execute();
 		return $movies;
 	}
@@ -44,15 +44,24 @@ class MoviesCrud {
     public function create($title, $altTitle, $director, $country, $year) {
 		$sql = 'INSERT INTO `films` (`title`, `altTitle`, `director`, `country`, `year`) VALUES (:title, :altTitle, :director, :country, :year)';
 		$stm = $this->db->prepare($sql);
-        $stm->bindparam(":title", $title->getTitle());
-		$stm->bindparam(":altTitle",$altTitle->getAltTitle());
-		$stm->bindparam(":director",$director->getDirector());
-		$stm->bindparam(":country",$country->getCountry());
-		$stm->bindparam(":year",$year->getYear());
+        $stm->bindparam(":title", $title->movie->getTitle());
+		$stm->bindparam(":altTitle",$altTitle->movie->getAltTitle());
+		$stm->bindparam(":director",$director->movie->getDirector());
+		$stm->bindparam(":country",$country->movie->getCountry());
+		$stm->bindparam(":year",$year->movie->getYear());
 		if (!empty($movies=$stm->execute())) {
             return $movies;
         }
         else
 		    return false; //@TODO
 	}
+
+	public function delete($id) {
+	    $sql = 'DELETE FROM `films` WHERE `id`=:id';
+	    $stm = $this->db->prepare($sql);
+	    $stm->bindparam(":id", $id);
+	    $stm->execute();
+	    $stm->setFetchMode(PDO::FETCH_CLASS, 'Movies');
+	    return $stm->fetch();
+    }
 }
