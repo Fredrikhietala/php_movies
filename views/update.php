ccfcc81c9ab8242cb383e19802f9c585291012ea
Fredrikhietala <hietala.fredrik@gmail.com>
2017-04-18 15:43:14 +0200

    Update working in view: todo move to view

--- views/update.php
+++ views/update.php
@@ -1,53 +1,59 @@
 <?php 
 require "../model/productsModel.php";
-include "header.php";
+require "header.php";
 $update = new MoviesCrud($connection);
+$editMovie = new Movies($connection);
+$editMovie = $update->getById($_REQUEST['edit_id']);
 $updateMovie = new Movies($connection);
-$updateMovie = $update->getById($_REQUEST['edit_id']);
-//var_dump($updateMovie = $update->getById($_REQUEST['edit_id']));
-//var_dump($updateMovie->getTitle('edit_id'));
+//var_dump($id = $_REQUEST['id']);
 
 if (isset($_POST['btn-update'])) {
-    $id = $this->updateMovie->getId();
-    $title = $this->updateMovie->setTitle($_POST['title']);
-    $altTitle = $this->updateMovie->setAltTitle($_POST['altTitle']);
-    $director = $this->updateMovie->setDirector($_POST['director']);
-    $country = $this->updateMovie->setCountry($_POST['country']);
-    $year = $this->updateMovie->setYear($_POST['year']);
+    $id = $_REQUEST['id'];
+    $title = $_POST['title'];
+    $updateMovie->setTitle($title);
+    $altTitle = $_POST['altTitle'];
+    $updateMovie->setAltTitle($altTitle);
+    $director = $_POST['director'];
+    $updateMovie->setDirector($director);
+    $country = $_POST['country'];
+    $updateMovie->setCountry($country);
+    $year = $_POST['year'];
+    $updateMovie->setYear($year);
 
     if ($movies = $update->update($id, $title, $altTitle, $director, $country, $year)) {
         echo "<p>Record was successfully updated</p>";
-        header("Location: index.php");
-        //include "views/show.php";
+        header("Location: ../index.php");
     }
     else {
         echo "Error";
         header("Location: ../views/update.php");
-        //include "views/show.php";
     }
 }
 ?>
 
 <form action="update.php" method="post">
 	<div class="form-group">
+        <input type="hidden" name="id" class="form-control" id="id" value="<?php echo $editMovie->getId(); ?>" readonly>
+    </div>
+    <div class="form-group">
 		<label for="title">Title: </label>
-		<input type="text" name="title" class="form-control" id="title" value="<?php echo $updateMovie->getTitle(); ?>" required>
+		<input type="text" name="title" class="form-control" id="title" value="<?php echo $editMovie->getTitle(); ?>" required>
 	</div>
 	<div class="form-group">
 		<label for="altTitle">Alternative title: </label>
-		<input type="text" name="altTitle" class="form-control" id="altTitle" value="<?php echo $updateMovie->getAltTitle(); ?>">
+		<input type="text" name="altTitle" class="form-control" id="altTitle" value="<?php echo $editMovie->getAltTitle(); ?>">
 	</div>
 	<div class="form-group">
 		<label for="director">Director: </label>
-		<input type="text" name="director" class="form-control" id="director" value="<?php echo $updateMovie->getDirector(); ?>" required>
+		<input type="text" name="director" class="form-control" id="director" value="<?php echo $editMovie->getDirector(); ?>" required>
 	</div>
 	<div class="form-group">
 		<label for="country">Country: </label>
-		<input type="text" name="country" class="form-control" id="country" value="<?php echo $updateMovie->getCountry(); ?>" required>
+		<input type="text" name="country" class="form-control" id="country" value="<?php echo $editMovie->getCountry(); ?>" required>
 	</div>
 	<div class="form-group">
 		<label for="year">Year: </label>
-		<input type="text" name="year" class="form-control" id="year" value="<?php echo $updateMovie->getYear(); ?>" required>
+		<input type="text" name="year" class="form-control" id="year" value="<?php echo $editMovie->getYear(); ?>" required>
 	</div>
 	<button type="submit" class="btn btn-default" name="btn-update">Update</button>
 </form>
