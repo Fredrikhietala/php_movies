af7a55ee18b9a4c9df65d1d3b27f65e0c37dfa54
Fredrikhietala <hietala.fredrik@gmail.com>
2017-04-19 09:43:15 +0200

    Added a start page for the project

---
+++ views/start.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fredrik Hietala
+ * Date: 2017-04-19
+ * Time: 09:38
+ */
+require "header.php";
+?>
+<h1>Welcome</h1>
+<p>Welcome to my movie-database, here you can see a list of my favorite movies.</p>
+
+<?php
+require "footer.php";
+?>
