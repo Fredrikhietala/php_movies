9b00fc125493b6b16e5308c28deeeceef54c471d
Fredrikhietala <hietala.fredrik@gmail.com>
2017-04-19 10:29:35 +0200

    Reconstructed the index-file

--- index.php
+++ index.php
@@ -4,40 +4,46 @@ require "model/productsModel.php";
 $obj = new MoviesCrud($connection);
 $movie = new Movies($connection);
 
-
-if (!empty($movie)) {
-    $movie = $obj->readAll();
-    require "views/show.php";
-
-    if (isset($_POST['btn-delete'])) {
-        $id = $_REQUEST['delete'];
-        $movie = $obj->delete($id);
-        header("Location: index.php");
+$qs = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_SPECIAL_CHARS);
+    if (empty($qs)) {
+        require_once "views/start.php";
     }
-}
-
-if (isset($_POST['btn-create'])) {
-
-    if (isset($_POST['insert'])) {
-        require "views/create.php";
-        $title = $_POST['title'];
-        $movie->setTitle($title);
-        $altTitle = $_POST['altTitle'];
-        $movie->setAltTitle($altTitle);
-        $director = $_POST['director'];
-        $movie->setDirector($director);
-        $country = $_POST['country'];
-        $movie->setCountry($country);
-        $year = $_POST['year'];
-        $movie->setYear($year);
-        var_dump($movies = $obj->create($title, $altTitle, $director, $country, $year));
-
-        if ($movies = $obj->create($title, $altTitle, $director, $country, $year)) {
-            echo "<div class='alert alert-success'>Movie was inserted</div>";
-            header("Location: index.php");
-        } else {
-            echo "<div class='alert alert-danger'>Unable to insert movie</div>";
-            header("Location: views/create.php");
+    elseif ($qs === "show") {
+        $movie = $obj->readAll();
+        require_once "views/show.php";
+        if (isset($_POST['btn-delete'])) {
+            $id = $_REQUEST['delete'];
+            $movie = $obj->delete($id);
+            header("Location: index.php?page=show");
         }
     }
-}
+    elseif ($qs === "create") {
+        require_once "views/create.php";
+        if (isset($_POST['insert'])) {
+            $title = $_POST['title'];
+            $movie->setTitle($title);
+            $altTitle = $_POST['altTitle'];
+            $movie->setAltTitle($altTitle);
+            $director = $_POST['director'];
+            $movie->setDirector($director);
+            $country = $_POST['country'];
+            $movie->setCountry($country);
+            $year = $_POST['year'];
+            $movie->setYear($year);
+
+            if ($movies = $obj->create($title, $altTitle, $director, $country, $year)) {
+                echo "<div class='alert alert-success'>Movie was inserted</div>";
+                header("Location: index.php?page=show");
+            } else {
+                echo "<div class='alert alert-danger'>Unable to insert movie</div>";
+                header("Location: index.php?page=create");
+            }
+        }
+    }
+    elseif ($qs === "update"){
+        require_once "views/update.php";
+
+    }
+    else
+        require_once "views/start.php";
+
