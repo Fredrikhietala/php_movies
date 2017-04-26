97e4da500eadfd1d77bea00a67f6f8e8452a7779
Fredrikhietala <hietala.fredrik@gmail.com>
2017-04-26 12:00:11 +0200

    Added a setId

--- model/Movie.php
+++ model/Movie.php
@@ -25,6 +25,9 @@ class Movie {
 	public function getId() {
 		return $this->id;
 	}
+    public function setId($id) {
+        $this->id = $id;
+    }
 	public function getTitle() {
 		return $this->title;
 	}
