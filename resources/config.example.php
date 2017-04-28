718397d72dd25438de7eff09bd45337096429bb4
Fredrikhietala <hietala.fredrik@gmail.com>
2017-04-28 15:00:12 +0200

    Changed example config-file

--- resources/config.example.php
+++ resources/config.example.php
@@ -1,18 +1,9 @@
 <?php
 
-	$host = ''; 
-	$db = '';
-	$user = '';
-	$password = '';
-	$charset = '';
-	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-	$options = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-				PDO::ATTR_EMULATE_PREPARES   => false  ];
-	try { 
-	//Try to connect to database
-	$connection = new PDO($dsn, $user, $password, $options);
-	} catch (PDOException $e){ 
-	//if not connected succesfully, give me an error message
-		echo $e->getMessage();
-	}
+return [
+    'db_type' => 'mysql',
+    'db_host' => '',
+    'db_name' => '',
+    'db_user' => '',
+    'db_password' => ''
+];
