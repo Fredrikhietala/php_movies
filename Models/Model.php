3509ba6839143c21b4a268a94ef5ea3b42bc15cb
Fredrikhietala <hietala.fredrik@gmail.com>
2017-05-03 20:57:07 +0200

    Removed namespaces

--- Models/Model.php
+++ Models/Model.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Models;
+//namespace Models;
 
-use PDO;
+//use PDO;
 
 class Model {
 	private $pdo;
--- Models/Movie.php
+++ Models/Movie.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Models;
+//namespace Models;
 
 class Movie {
 
