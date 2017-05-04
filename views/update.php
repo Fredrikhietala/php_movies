c094303a9853154227cf1c082c9b53cbeb3b8ae0
Fredrikhietala <hietala.fredrik@gmail.com>
2017-05-04 21:40:05 +0200

    Trying to get getById function to work

--- views/update.php
+++ views/update.php
@@ -1,34 +1,39 @@
 <?php
+/* @var Controller $this */
 require "header.php";
+$id = $_POST['edit'];
+foreach ($this->getById($id) as $row) :
+/* @var Movie $row */
 ?>
 
 <form action="../index.php?page=update" method="post">
 	<div class="form-group">
-        <input type="hidden" name="id" class="form-control" id="id" value="<?php echo $movie->getId(); ?>" readonly>
+        <input type="hidden" name="id" class="form-control" id="id" value="<?php echo $row->getId(); ?>" readonly>
     </div>
     <div class="form-group">
 		<label for="title">Title: </label>
-		<input type="text" name="title" class="form-control" id="title" value="<?php echo $movie->getTitle(); ?>" required>
+		<input type="text" name="title" class="form-control" id="title" value="<?php echo $row->getTitle(); ?>" required>
 	</div>
 	<div class="form-group">
 		<label for="altTitle">Alternative title: </label>
-		<input type="text" name="altTitle" class="form-control" id="altTitle" value="<?php echo $movie->getAltTitle(); ?>">
+		<input type="text" name="altTitle" class="form-control" id="altTitle" value="<?php echo $row->getAltTitle(); ?>">
 	</div>
 	<div class="form-group">
 		<label for="director">Director: </label>
-		<input type="text" name="director" class="form-control" id="director" value="<?php echo $movie->getDirector(); ?>" required>
+		<input type="text" name="director" class="form-control" id="director" value="<?php echo $row->getDirector(); ?>" required>
 	</div>
 	<div class="form-group">
 		<label for="country">Country: </label>
-		<input type="text" name="country" class="form-control" id="country" value="<?php echo $movie->getCountry(); ?>" required>
+		<input type="text" name="country" class="form-control" id="country" value="<?php echo $row->getCountry(); ?>" required>
 	</div>
 	<div class="form-group">
 		<label for="year">Year: </label>
-		<input type="text" name="year" class="form-control" id="year" value="<?php echo $movie->getYear(); ?>" required>
+		<input type="text" name="year" class="form-control" id="year" value="<?php echo $row->getYear(); ?>" required>
 	</div>
 	<button type="submit" class="btn btn-default" name="btn-update">Update</button>
 </form>
 
 <?php
+endforeach;
 require "footer.php";
 ?>
