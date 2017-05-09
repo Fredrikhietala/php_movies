e67971305cc94e76902d7d751dc1b527b9746bc2
Fredrikhietala <hietala.fredrik@gmail.com>
2017-05-09 15:46:03 +0200

    CSS changes

--- views/header.php
+++ views/header.php
@@ -10,8 +10,8 @@
 
     <title>Filmer</title>
 
-    <link rel="stylesheet" type="text/css" href="../resources/main.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../resources/main.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
