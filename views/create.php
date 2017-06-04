297a960f927f3ded74695c2b4d1635078ca6e520
Fredrikhietala <hietala.fredrik@gmail.com>
2017-06-04 21:40:27 +0200

    Added a select-list for create a movie-form

--- views/create.php
+++ views/create.php
@@ -1,19 +1,28 @@
 <?php
+/* @var Controller $this */
 require "header.php";
 ?>
 
 <form action="/index.php?page=create" method="post">
-	<div class="form-group">
+    <div class="form-group">
+        <label for="director">Director: </label>
+        <select class="form-control" id="director" name="director">
+            <option value="">Choose Director</option>
+            <?php
+            foreach ($this->readAllDirectors() as $rows) :
+                /* @var Director $rows */
+            ?>
+            <option value="<?php echo $rows->getName() ?>"><?php echo $rows->getName() ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+    <div class="form-group">
 		<label for="title">Title: </label>
         <input type="text" name="title" class="form-control" id="title" placeholder="Title" required>
 	</div>
 	<div class="form-group">
 		<label for="altTitle">Alternative title: </label>
 		<input type="text" name="alt_title" class="form-control" id="altTitle" placeholder="Alternative title">
-	</div>
-	<div class="form-group">
-		<label for="director">Director: </label>
-		<input type="text" name="director" class="form-control" id="director" placeholder="Director" required>
 	</div>
 	<div class="form-group">
 		<label for="country">Country: </label>
