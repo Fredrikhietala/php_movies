7e3975ce36764123266976a5da7f285481f80c15
Fredrikhietala <hietala.fredrik@gmail.com>
2017-06-05 15:16:13 +0200

    Added a new views to show movies

---
+++ views/show_movies.php
@@ -0,0 +1,34 @@
+<?php
+/* @var Model $model */
+/* @var Director $director */
+require "header.php";
+?>
+<h2>Movies by <?php echo $director->getName(); ?></h2>
+<table class="table-striped">
+    <tr>
+        <th>Title</th>
+        <th>Alternative Title</th>
+        <th>Year</th>
+        <th>Delete</th>
+        <th>Update</th>
+    </tr>
+    <?php
+    /* @var Model $id */
+    foreach ($this->model->readMovies($id) as $movies):
+        /* @var Movie $movies */
+        ?>
+        <tr>
+            <td><?php echo $movies->getTitle(); ?></td>
+            <td><?php echo $movies->getAltTitle(); ?></td>
+            <td><?php echo $movies->getYear(); ?></td>
+            <td>
+                <a class="btn btn-default" href="/index.php?page=update_movie&id=<?php echo $movies->getId(); ?>">Update</a>
+            </td>
+            <td>
+                <a class="btn btn-default" href="/index.php?page=delete_movie&id=<?php echo $movies->getId(); ?>">Delete</a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+<br>
+<a class="btn btn-default" href="/index.php?page=show">Back</a>
